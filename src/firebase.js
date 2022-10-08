6227e82840ede709ae4397b398fa992738d88c29
Novacovici Sonia <sonianovacovici@gmail.com>
2022-10-08 21:34:01 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFsK2pQ8l_4TQIfBr2tcwPh5gugYYM87I",
+  authDomain: "school-project-45105.firebaseapp.com",
+  projectId: "school-project-45105",
+  storageBucket: "school-project-45105.appspot.com",
+  messagingSenderId: "593553250452",
+  appId: "1:593553250452:web:1307333353028842efbc81",
+  measurementId: "G-LMGK28X13W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const firestore = getFirestore(app);
